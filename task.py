e79896bdfb84ddcf3a32a7678e77e7ca71ea8d3b
marcoanaya <35743888+marcoanaya@users.noreply.github.com>
2018-11-15 13:03:41 -0600

    Updated task.py
    Made they file paths more general and added a brief description and clarification

--- task.py
+++ task.py
@@ -1,12 +1,14 @@
 # based on the three experiments!/usr/bin/env python
 # coding=utf-8
 
+#this script is essentially a command-line command in script form
+
 from aeneas.tools.execute_task import ExecuteTaskCLI
 
 ExecuteTaskCLI(use_sys=False).run(arguments=[
     None,
-    u"test_videos/test-trim-sox.mp3",
-    u"test_file/test-trim1.txt",
+    u"path/to/audio.mp3", #.mp3 , .txt, and .csv are just examples, since more file types are supported
+    u"path/to/text.txt", 
     u"task_language=eng|is_text_type=plain|os_task_file_format=csv|task_adjust_boundary_nonspeech_min=0.010|task_adjust_boundary_nonspeech_string=REMOVE|task_adjust_boundary_algorithm=rateaggressive|task_adjust_boundary_rate_value=14.000",
-    u"output/test-map2.csv"
+    u"path/to/output.csv"
 ])
