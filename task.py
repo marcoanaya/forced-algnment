6fdc5a1a7ae0d1dffd9318ef322d9f0f17ea07f4
marcoanaya <35743888+marcoanaya@users.noreply.github.com>
2018-12-18 20:17:36 -0600

    Update task.py

--- task.py
+++ task.py
@@ -1,14 +1,22 @@
 # based on the three experiments!/usr/bin/env python
 # coding=utf-8
 
-#this script is essentially a command-line command in script form
+from aeneas.executetask import ExecuteTask
+from aeneas.task import Task
 
-from aeneas.tools.execute_task import ExecuteTaskCLI
+config_string =(
+    u"task_language=eng|" + 
+    u"is_text_type=plain|" + #
+    u"os_task_file_format=csv|" + # output file type
+    u"task_adjust_boundary_nonspeech_min=0.010|" + # consider timestamps of nonspeech if they are longer than .10 sec
+    u"task_adjust_boundary_nonspeech_string=REMOVE|" + # remove nonspeech timestamps
+    u"task_adjust_boundary_algorithm=rateaggressive|" + 
+    u"task_adjust_boundary_rate_value=14.000")
+task = Task(config_string=config_string)
+task.audio_file_path_absolute = u"audio/test-sox-low.mp3"
+task.text_file_path_absolute = u"transcripts/test.txt"
+task.sync_map_file_path_absolute = u"output/test-sox3.csv"
 
-ExecuteTaskCLI(use_sys=False).run(arguments=[
-    None,
-    u"path/to/audio.mp3", #.mp3 , .txt, and .csv are just examples, since more file types are supported
-    u"path/to/text.txt", 
-    u"task_language=eng|is_text_type=plain|os_task_file_format=csv|task_adjust_boundary_nonspeech_min=0.010|task_adjust_boundary_nonspeech_string=REMOVE|task_adjust_boundary_algorithm=rateaggressive|task_adjust_boundary_rate_value=14.000",
-    u"path/to/output.csv"
-])
+ExecuteTask(task).execute()
+
+task.output_sync_map_file()
