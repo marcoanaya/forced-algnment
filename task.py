7b41161a7d2f9fc9b905d7df1dca1dd9642de690
marcoanaya <marco.a.anaya34@gmail.com>
2018-12-20 16:52:20 -0600

    updated task.py

--- task.py
+++ task.py
@@ -4,6 +4,13 @@
 from aeneas.executetask import ExecuteTask
 from aeneas.task import Task
 
+# This script could provide additional functionality by:
+#    -automatically taking a video and converting it to an audio file
+#    -interactively asking the user for timestamps to create a "noise profile"
+#    -use that profile to run SoX "noisered", creating an audio file with reduced background noise
+#    -retrieving transcript from database as a text file
+
+
 config_string =(
     u"task_language=eng|" + 
     u"is_text_type=plain|" + #
@@ -13,6 +20,7 @@ config_string =(
     u"task_adjust_boundary_algorithm=rateaggressive|" + 
     u"task_adjust_boundary_rate_value=14.000")
 task = Task(config_string=config_string)
+# currently the task takes predetermined audio, transcript and output paths
 task.audio_file_path_absolute = u"audio/test-sox-low.mp3"
 task.text_file_path_absolute = u"transcripts/test.txt"
 task.sync_map_file_path_absolute = u"output/test-sox3.csv"
